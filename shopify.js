fbe3bf3426861501982d54396b2c81c5708405cb
Matthew Davis <lenixx@gmail.com>
2016-07-18 22:39:08 +0000

    added search and csv

--- shopify.js
+++ shopify.js
@@ -4,12 +4,17 @@ var request = require('request-json');
 var async = require('async');
 var argv = require('minimist')(process.argv.slice(2));
 var verbose = argv['verbose'] || false;
+var jsonSearch = require('json-search');
+var converter = require('json-2-csv');
 
 var jsonFormat = require('json-format');
 
 var domain = argv['domain'] || false;
 var req = request.createClient('https://' + domain);
 var url = '';
+var optsMethod = argv['method'] || false;
+var optsSaveDir = argv['savedir'] || __dirname;
+var optsSearch = argv['search'] || false;
 var optsCollection = argv['collection'] || false;
 var curCollection;
 
@@ -48,7 +53,7 @@ if(optsCollection) {
     
     shopifyDump(domain, function(err, masterHash) { // callback to execute with hash of products
     
-        fs.writeFileSync(__dirname + '/products.json', JSON.stringify(masterHash));
+        fs.writeFileSync(optsSaveDir + '/' + domain + '.products.json', JSON.stringify(masterHash));
         
     }, {
         
@@ -68,12 +73,21 @@ if(optsCollection) {
     
 }
 
+function search(terms) {
+    
+    var javascriptProgrammers = rjsonSearch.find(programmers, 'javascript');
+    
+    console.log(javascriptProgrammers.length);
+    
+}
 
 function parseCollection(collection, body) { 
 //
 // This is called each time a collection is downloaded.
 //
     console.log('Parsing Collection: ' + domain + ': ' + collection + ' (Products: ' + body.products.length + ")");
+
+var searchResults = [];
 
     if(body.products.length > 0) {
 
@@ -84,24 +98,78 @@ function parseCollection(collection, body) {
             
             if(verbose) console.log("\t" + collection + ": " + body.products[i].title);
             
+            body.products[i].link = 'https://' + domain + '/products/' +  body.products[i].handle;
+             
+            if(optsSearch) {
+                
+                var s = JSON.stringify(body.products[i]);
+                
+                if(s.search(optsSearch) > 0) {
+                    
+                    searchResults.push(body.products[i]);
+
+                        console.log("\t" + collection + ": " + body.products[i].title + ' (' + body.products[i].link + ')');
+                                        
+                }
+                
+            }
+            
         }
         
+    // console.log(body);
+    
         //
         // Save each product collection to a separate file.
         //
         // fs.writeFileSync(__dirname + domain + '/' + '/products/' + type + '.json', JSON.stringify(body.products));
         
         // var fileName = __dirname + '/' + domain + '/' + '/products/' + collection + '.json';
-        var fileName = __dirname + '/' + domain + '.' + collection + '.json';
+        var fileName;
         
-        fs.writeFile(fileName, jsonFormat(body.products), function(err){
-        
-            // if (err) throw err;
-        
-            if(verbose) console.log('Saved collection to: ' + fileName);
+        var jsonArr = body.products;
+        var options = {
+                
+            delimiter : {
+                wrap  : '"', // Double Quote (") character
+                field : ',', // Comma field delimiter
+                array : ';', // Semicolon array value delimiter
+                eol   : '\n' // Newline delimiter
+            },
+            prependHeader    : true,
+            sortHeader       : false,
+            trimHeaderValues : true,
+            trimFieldValues  :  true,
+            keys             : ['id', 'product_type', 'vendor', 'title', 'handle', 'link','published_at', 'created_at','updated_at']
             
-        });
+        };
+        
+        if(optsSearch && searchResults.length > 0) {
+        
+            jsonArr = searchResults;
+            fileName = optsSaveDir + '/' + domain + '.' + collection + '.search.' + optsSearch + '.csv';
+
+        } else {
             
+            fileName = optsSaveDir + '/' + domain + '.' + collection + '.csv';
+            
+        }
+
+        var json2csvCallback = function (err, csv) {
+            
+            if (err) throw err;
+            
+            fs.writeFile(fileName, csv, function(err){
+        
+                // if (err) throw err;
+            
+                if(verbose) console.log('Saved collection to: ' + fileName);
+                
+            });
+            
+        };
+
+        converter.json2csv(jsonArr, json2csvCallback, options);
+
     }
     
 }
